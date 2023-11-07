026439efa2d22800013ad8adfdd831e54081d427
MaxDac <massimiliano.dacunzo@hotmail.com>
2023-11-07 18:02:50 +0000

    Added form

--- SomeBlazorApp/Components/Pages/CardExample.razor.cs
+++ SomeBlazorApp/Components/Pages/CardExample.razor.cs
@@ -1,15 +1,26 @@
-﻿namespace SomeBlazorApp.Components.Pages;
+﻿using SomeBlazorApp.Models;
+
+namespace SomeBlazorApp.Components.Pages;
 
 public partial class CardExample
 {
+    public FormModel? Model { get; set; }
+
 	public static string? Something { get; private set; }
 
 	protected override async Task OnInitializedAsync()
 	{
 		await base.OnInitializedAsync();
+		
+		Model ??= new();
 
-		await Task.Delay(1000);
+		await Task.Delay(5000);
 
 		Something = "Something else";
+	}
+	
+	private void Submit()
+	{
+		logger.LogInformation($"The name: {Model?.Name}");
 	}
 }
---
+++ SomeBlazorApp/Models/FormModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SomeBlazorApp.Models;
+
+public class FormModel
+{
+    public string Name { get; set; } = "";
+    
+    public bool Selected { get; set; } = false;
+    
+    public string Order { get; set; } = "1";
+}
