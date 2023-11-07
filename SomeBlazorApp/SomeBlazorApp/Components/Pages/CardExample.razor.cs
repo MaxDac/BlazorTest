297b163f58ad734dc3e98f37e5817257bb84c110
MaxDac <massimiliano.dacunzo@hotmail.com>
2023-11-07 16:49:43 +0000

    First commit

---
+++ SomeBlazorApp/SomeBlazorApp/Components/Pages/CardExample.razor.cs
@@ -0,0 +1,16 @@
+﻿
+namespace SomeBlazorApp.Components.Pages;
+
+public partial class CardExample
+{
+	public static string? Something { get; private set; }
+
+	protected override async Task OnInitializedAsync()
+	{
+		await base.OnInitializedAsync();
+
+		await Task.Delay(1000);
+
+		Something = "Something else";
+	}
+}
